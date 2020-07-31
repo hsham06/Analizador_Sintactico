20ea257e5c5ef5950486707b7dd36b20d2a41a04
Hanvan Sham <hsham@gcs-systems.com>
2020-07-30 23:11:11 -0400

    Analizador Sintactico

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author Charly Ponce
+ */
+public enum Tokens {
+    Linea,
+    Comillas,
+    T_dato,
+    Cadena,
+    If,
+    Else,
+    Do,
+    While,
+    For,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Op_logico,
+    Op_incremento,
+    Mayor,
+    Mayor_Igual,
+    Menor,
+    Menor_Igual,
+    No_Igual,
+    Op_atribucion,
+    Op_booleano,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Corchete_a,
+    Corchete_c,
+    Main,
+    P_coma,
+    Identificador,
+    Numero,
+    ERROR
+}
